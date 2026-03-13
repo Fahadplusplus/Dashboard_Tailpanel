d952ece0673ac405e249d6ebefa7d7baf646da25
Fahadplusplus <nasirfahad420@gmail.com>
2026-03-13 10:44:03 +0500

    Fixed problems caused by minor chnages commit

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -3,6 +3,7 @@ import Sidebar from "../components/layout/Sidebar";
 import Topbar from "../components/layout/Topbar"
 import Counter from "../components/layout/Counter"
 import DashboardCard from "../components/layout/DashboardCard";
+import Dashboard from "../pages/Dashboard";
 import "../styles/Layout.css";
 
 export default function Layout() {
@@ -40,12 +41,7 @@ export default function Layout() {
       );
     }
   };
-  const cardsData = [
-    { id: 1, cardTitle: "Total Revenue", cardSubTitle: "$45,231.89", cardText: "vs last month", percent: "20.1%", icon: "bi bi-currency-dollar" },
-    { id: 2, cardTitle: "Active Users", cardSubTitle: "2,350", cardText: "vs last month", percent: "15.3%", icon: "bi bi-currency-dollar" },
-    { id: 3, cardTitle: "Total Orders", cardSubTitle: "560", cardText: "vs last month", percent: "4.2%", icon: "bi bi-currency-dollar" },
-    { id: 4, cardTitle: "Converstion Rate", cardSubTitle: "3.42%", cardText: "vs last month", percent: "8.7%", icon: "bi bi-currency-dollar" }
-  ];
+ 
 
   return (
     <div className="layout">
@@ -61,22 +57,9 @@ export default function Layout() {
 
         <div className="content">
 
-          <h1>Dashboard Content</h1>
-          {/* <Counter /> */}
-          {/* <DashboardCard cardTitle="fahad" cardSubTitle="Nasir" cardText="Abacus" percent="20.1%" icon="bi bi-star" /> */}
-          
-          <div className="row">
-            {cardsData.map((card) => (
-              <div className="col-12 col-md-6 col-xxl-3 g-3" key={card.id}>
-                <DashboardCard
-                  cardTitle={card.cardTitle}
-                  cardSubTitle={card.cardSubTitle}
-                  cardText={card.cardText}
-                  icon={card.icon}
-                />
-              </div>
-            ))}
-          </div>
+        
+          <Dashboard />
+         
         </div>
 
       </div>
