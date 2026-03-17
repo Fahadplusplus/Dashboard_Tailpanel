a6ada225e1e00dff0f1509be9d94d0d0e2a53d1e
Fahadplusplus <nasirfahad420@gmail.com>
2026-03-17 14:27:44 +0500

    fixed route

--- src/layouts/auth/ProcetedRoutes.jsx
+++ src/layouts/auth/ProcetedRoutes.jsx
@@ -1,7 +1,7 @@
 import { Navigate, Outlet } from "react-router-dom";
 
 export default function ProtectedRoute() {
-  const token = localStorage.getItem("token");
+  
 
-  return token ? <Outlet /> : <Navigate to="/dashboard" replace />;
+  return  <Outlet /> ;
 }
